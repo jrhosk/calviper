fa9c9b2d294813963a89c066cd3ab58547b5eb0b
Joshua Hoskins <jrh4dy@gmail.com>
2024-10-16 11:32:37 -0400

    Updated skeleton of a calibration factory for the gain table to include working empty_like(...) function and generate(...) functions.

--- src/calviper/table.py
+++ src/calviper/table.py
@@ -1,3 +1,4 @@
+import numpy as np
 import xarray as xr
 
 import toolviper.utils.logger as logger
@@ -22,12 +23,45 @@ class CalibrationFactory(ABC):
 
 class GainTable(BaseCalibrationTable):
 
-    # Specific implementation of gain table
-    def generate(self, coords: dict)-> Union[xr.Dataset, None]:
-        pass
+    # This is intended to be an implementation of a gain table simulator. It is
+    # currently very rough and filled with random numbers. Generally based on the
+    # original cal.py
+    def generate(self, coords: dict)-> xr.Dataset:
+        shape = tuple(value.shape[0] for value in coords.values())
 
-    def empty_like(self, dataset: xr.Dataset)->Union[xr.Dataset, None]:
-        pass
+        dims = {}
+        for key, value in coords.items():
+            dims[key] = value.shape[0]
+
+        parameter = np.random.uniform(-np.pi, np.pi, shape)
+        amplitude = np.random.normal(1.0, 0.1, shape)
+        parameter = np.vectorize(complex)(
+            np.cos(parameter),
+            np.sin(parameter)
+        )
+
+        xds = xr.Dataset()
+
+        xds["PARAMETER"] = xr.DataArray(amplitude * parameter, dims=dims)
+        xds = xds.assign_coords(coords)
+
+        return xds
+
+
+    @staticmethod
+    def empty_like(dataset: xr.Dataset)->xr.Dataset:
+        dims = list(dataset.sizes)
+        parameter = np.empty(tuple(dataset.sizes.values()))
+
+        xds = xr.Dataset()
+
+        xds["PARAMETER"] = xr.DataArray(parameter, dims=dims)
+        xds.attrs["calibration_type"] = "gain"
+
+        xds = xds.assign_coords(dataset.coords)
+
+        return xds
+
 
 class CalibrationTable(CalibrationFactory):
 
